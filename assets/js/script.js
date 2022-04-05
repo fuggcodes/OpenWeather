c52d9c4c920a225f4a5ebdd629c64cd05b8d516a
David Binyatov <davidbphotography101@gmail.com>
2022-04-04 22:47:14 -0400

    display weather icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,25 @@ function searchCity(cityName) {
         var hum = $("<p>").text("Humidity: " + response.main.humidity);
         var wind = $("<p>").text("Wind Speed: " + response.wind.speed);
         var currentweather = response.weather[0].main;
+
+        if (currentweather === "Rain") {
+            var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/09d.png");
+            currentIcon.attr("style", "height: 60px; width: 60px");
+        } 
+        else if (currentweather === "Clouds") {
+            var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/03d.png");
+            currentIcon.attr("style", "height: 60px; width: 60px");
+        } 
+        else if (currentweather === "Clear") {
+            var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/01d.png");
+            currentIcon.attr("style", "height: 60px; width: 60px");
+        }
+        else if (currentweather === "Drizzle") {
+            var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/10d.png");
+            currentIcon.attr("style", "height: 60px; width: 60px");
+        }
+        else if (currentweather === "Snow") {
+            var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/13d.png");
+            currentIcon.attr("style", "height: 60px; width: 60px");
+        }
+        
